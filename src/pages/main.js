252ea737f6765ba6d1a3d36c1e7a42d076205d2e
Administrator <17tucs134@skct.edu.in>
2019-08-30 16:08:03 +0530

    my commit

---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ export default class MainPage extends React.Component{
+render()
+{
+return (
+<div>
+<p>Name</p>
+<p>College Name</p>
+<p>Year</p>
+<p>Department</p></div>
+)
+}
+}
